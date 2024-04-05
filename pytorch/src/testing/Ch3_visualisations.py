5d4d35f4c4f06b0fbe66634c4256e0622d5308eb
Fergal Riordan <riordanf@tcd.ie>
2024-04-05 22:11:10 +0100

    Chapter 3 metric visualisations

--- pytorch/src/testing/Ch3_visualisations.py
+++ pytorch/src/testing/Ch3_visualisations.py
@@ -2,22 +2,22 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Load your dataframes
-df1 = pd.read_csv('pytorch/src/testing/DnCNN.csv')
-df2 = pd.read_csv('pytorch/src/testing/UNet.csv')
-df3 = pd.read_csv('pytorch/src/testing/ResNet.csv')
+df1 = pd.read_csv('pytorch/src/testing/Ch3_metric_data/DnCNN.csv')
+df2 = pd.read_csv('pytorch/src/testing/Ch3_metric_data/UNet.csv')
+df3 = pd.read_csv('pytorch/src/testing/Ch3_metric_data/ResNet.csv')
 
 # Set up the plotting
 plt.figure(figsize=(8, 5))
 
-plt.plot(df3['Epoch'], df3['FID_N_mean'], label='Night Images')
-plt.plot(df3['Epoch'], df3['FID_D_mean'], label='Day Images')
+plt.plot(df1['Epoch'], df1['FID_N_mean'], label='Night Images')
+plt.plot(df1['Epoch'], df1['FID_D_mean'], label='Day Images')
 
 # Shade the area between the two lines
-plt.fill_between(df3['Epoch'], df3['FID_N_mean'], df3['FID_D_mean'], color='grey', alpha=0.3)
+plt.fill_between(df1['Epoch'], df1['FID_N_mean'], df1['FID_D_mean'], color='grey', alpha=0.3)
 
 plt.ylim(100, 200)
 
-plt.title('Mean FID values for ResNet-18 encoded generator')
+plt.title('Mean FID values for Original CycleGAN generator')
 plt.xlabel('Epochs')
 plt.ylabel('FID')
 plt.legend()
